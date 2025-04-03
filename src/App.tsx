da3f4d9136d7857c19b28cef65052d905e0a30a2
jhoonyeel <jhoonyeel@gmail.com>
2025-04-04 06:43:07 +0900

    chore: Tailwind 설정 후 정상 동작 확인

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="bg-red-500 text-white p-4">Hello, Tailwind!</h1>
+    </div>
+  );
+};
+
+export default App;
